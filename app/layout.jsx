ff192762868ef2903e0670373c276f1394ca6be6
DevarajuAlugolu <alugoludevaraju26@gmail.com>
2025-11-25 14:11:39 +0530

    depoly ready

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,4 @@
 import "../styles/globals.css";
-
 import { FaLink } from "react-icons/fa6";
 
 export const metadata = {
@@ -9,8 +8,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className="h-full">
+      <body className="min-h-screen flex flex-col">
         <header className="gradient-bg text-white shadow-lg">
           <div className="container mx-auto px-4 py-4">
             <div className="flex justify-between items-center">
@@ -21,8 +20,12 @@ export default function RootLayout({ children }) {
             </div>
           </div>
         </header>
-        <main className="max-w-5xl mx-auto px-4 py-6">{children}</main>
-        <footer className="bg-gray-800 text-white py-8 mt-12">
+
+        <main className="flex-1 max-w-5xl mx-auto px-4 py-6 w-full">
+          {children}
+        </main>
+
+        <footer className="bg-gray-800 text-white py-8">
           <div className="container mx-auto px-4">
             <div className="flex flex-col md:flex-row justify-between items-center">
               <div className="mb-4 md:mb-0">
